5ea7012bfa08549dc338694a89d0dd512373a87e
strongmhk <113084655+strongmhk@users.noreply.github.com>
2023-02-12 21:29:14 +0900

    variable

---
+++ java/java1/variable.java
@@ -0,0 +1,19 @@
+public class variable{
+	public static void main(String[] args) {
+		// 대표적인 타입 정수, 실수, 문자열
+		int a = 1; // Number -> integer
+		System.out.println(a);
+		
+		double b = 1.1; // real number -> double
+		System.out.println(b);
+		
+		String c = "Hello World!";
+		System.out.println(c);
+		
+		
+		String name = "minhyung";
+		System.out.println("Hello, "+name+" ... "+name+" ... bye" ); // 변수의 값만 바뀌면 출력하는 문장을 바꿀수있음~~
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+}
